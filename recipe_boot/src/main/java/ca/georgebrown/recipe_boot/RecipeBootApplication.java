669b1b90314910dbd2c88e63578863675b1f2f5e
ostap_sulyk <ostapsulyk@gmail.com>
2022-10-15 14:08:42 -0400

    Initialized project

---
+++ recipe_boot/src/main/java/ca/georgebrown/recipe_boot/RecipeBootApplication.java
@@ -0,0 +1,13 @@
+package ca.georgebrown.recipe_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBootApplication.class, args);
+	}
+
+}
---
+++ recipe_boot/src/test/java/ca/georgebrown/recipe_boot/RecipeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.georgebrown.recipe_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
